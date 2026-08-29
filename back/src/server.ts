5beb1484542da6f8150ad5db2de8b1f59af5d739
kivitasku <158566850+kivitasku@users.noreply.github.com>
2026-08-29 18:29:11 +0300

    added declared methods, get post put delete

--- back/src/server.ts
+++ back/src/server.ts
@@ -37,6 +37,7 @@ server.register(fastifySecureSession, {
 server.register(cors, {
   origin: "http://localhost:5173",
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
 });
 
 const musicDirectoryEnv = process.env.MUSIC_RES;
