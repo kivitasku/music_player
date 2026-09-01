3141cfbc6e88c656680539b9f15d2d01df76d953
kivitasku <158566850+kivitasku@users.noreply.github.com>
2026-09-01 16:56:13 +0300

    almost deleted song info

--- front/src/components/Song.tsx
+++ front/src/components/Song.tsx
@@ -6,14 +6,20 @@ interface SongProps {
   song: SongType;
   onPlay: (song: SongType) => void;
   isAlbumPage?: boolean;
+  isSearchResult?: boolean;
   onSongMenuOpen: (song: SongType, isAlbumPage: boolean) => void;
+  onSelectArtist: (artistId: number) => void;
+  onSelectAlbum?: (albumId: number) => void;
 }
 
 export default function Song({
   song,
   onPlay,
   isAlbumPage = false,
+  isSearchResult = false,
   onSongMenuOpen,
+  onSelectArtist,
+  onSelectAlbum = () => {},
 }: SongProps) {
   
 
@@ -30,6 +36,34 @@ export default function Song({
 
       <div className="song-info">
         <h3>{song.title ?? "No song"}</h3>
+
+                {!isAlbumPage && (
+          <>
+
+            <p
+              className="song-artist-link"
+              onClick={() => onSelectArtist(song.artists.id)}
+              role="button"
+              tabIndex={0}
+            >
+              {song.artists.name ?? "No artist"}
+            </p>
+
+            <p
+              className="song-album-link"
+              onClick={() => {
+                if (song.albums) {
+                  onSelectAlbum(song.albums.id);
+                }
+              }}
+              role="button"
+              tabIndex={0}
+            >
+              {song.albums?.title ?? "No album"}
+            </p>
+        
+          </>
+        )}
 
 
       </div>
